9d74fe39daa6536d1c09aaeb147c19fa3d2beedd
Alex Defreitas <alexandreadef@gmail.com>
2016-04-26 12:21:17 -0400

    Added download dropdown to header

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,10 +1,12 @@
 var Dropdown = function() {
 
-  var dropdownButton = $('[data-ui-button]');
-  var foldout = $('[data-ui-foldout]');
+  var dropdownButton = $('[data-ui-button]')
 
-  dropdownButton.click(function(){
-    dropdownButton.toggleClass('active');
-    foldout.toggleClass('active');
+  dropdownButton.click(function() {
+    var elem = $(this).attr('data-ui-button')
+    var foldout = $('[data-ui-foldout="' + elem + '"]')
+
+    $(this).toggleClass('active')
+    foldout.toggleClass('active')
   })
 };
