14122b278cc15be502fd44dad37266c4816eb144
Matt McInerney <matt@motel.is>
2016-05-20 09:48:27 -0400

    copy updates

--- data/content.js
+++ data/content.js
@@ -3,42 +3,42 @@ var content = {
   "distractions" : {
     "title": "Feeling distracted & overwhelmed?",
     "foldout": "Digital distractions are a huge problem in our always-connected world. Our work is suffering from that stressful, steady stream of dings, pings and notifications.",
-    "button": "<span class='mobile-hidden'>Learn About </span>Distractions"
+    "button": "<span class='mobile-hidden'>Learn about </span>distractions"
   },
 
   "sessions" : {
-    "title": "I'll get you focused so you can work better",
+    "title": "I'll get you focused so you can work better...",
     "foldout": "I am designed to help you focus by working in timed blocks. This “timeboxing” rhythm is proven to help people do better and more creative work.",
-    "button": "<span class='mobile-hidden'>Learn About </span>Focus Sessions"
+    "button": "<span class='mobile-hidden'>Learn about </span>focus sessions"
   },
 
   "categorizing" : {
-    "title": "I'll block out Evil sites & apps",
+    "title": "I'll block out Evil sites & apps...",
     "foldout": "I’ll learn which of the sites and apps you use are ‘Good’ and which are  ‘Evil,’ and block the things that distract you, while rewarding you for using the tools you need.",
-    "button": "<span class='mobile-hidden'>Learn About </span>Distraction Blocking"
+    "button": "<span class='mobile-hidden'>Learn about </span>distraction blocking"
   },
 
   "challenges" : {
-    "title": "so you can build better work habits",
+    "title": "so you can build better work habits...",
     "foldout": "Using personalized Challenges, I’ll help you develop better work habits, so you can do things like beat that Facebook addiction or stop staying late at the office every night.",
-    "button": "<span class='mobile-hidden'>Learn About </span>Challenges"
+    "button": "<span class='mobile-hidden'>Learn about </span>challenges"
   },
 
   "breaks" : {
-    "title": "and I'll make sure you take breaks",
+    "title": "and I'll make sure you take breaks...",
     "foldout": "Studies show that taking adequate breaks during work can reduce stress and keep your mind agile and creative. I’ll make sure you break when you need to.",
-    "button": "<span class='mobile-hidden'>Learn About </span>Breaks"
+    "button": "<span class='mobile-hidden'>Learn about </span>breaks"
   },
 
   "life" : {
     "title": "so you can live a more fulfilling life.",
     "foldout": "Saent is building a platform to help you live a more fulfilling life. Coming soon: integrations with other apps and devices, personalized learning goals, inspirational content, and more.",
-    "button": "What Else Can Saent Do?"
+    "button": "What else can Saent do?"
   },
 
   "download" : {
     "title": " So give me a try and we’ll do amazing work together.",
     "foldout": "Saent is building a platform to help you live a more fulfilling life. Coming soon: integrations with other apps and devices, personalized learning goals, inspirational content, and more.",
-    "button": "What Else Can Saent Do?"
+    "button": "What else can Saent do?"
   }
 };
